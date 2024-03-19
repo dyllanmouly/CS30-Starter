1885de902bf88d32948d2c98e9c52a6eece2797c
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-03-19 11:56:18 -0600

    testing java
    tests n stuff

---
+++ testjava/testjava/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Wor\tld!");
+        System.out.println("Hello, Wor\nld!");
+        System.out.println("Hello, Wor\rld!");
+        System.out.println("Hello, Wor\bld!");
+        System.out.println("Hello, World!");
+    }
+}
