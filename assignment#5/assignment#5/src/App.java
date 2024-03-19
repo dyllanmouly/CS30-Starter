7571dcae939834b6836cdfb6bc2762e794ae6d91
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-03-19 11:57:08 -0600

    assignment #5
    text n stuffs

---
+++ assignment#5/assignment#5/src/App.java
@@ -0,0 +1,13 @@
+// Program/Assignment name: assignment#5
+// Student name: Dyllan Mouly
+// Date: March 19, 2024
+// Program Description: text n stuff
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Computer Science 30\r\n" + 
+                           "My Name is Dyllan and my favourite food is chips.\r\n" +
+                           "Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.\r\n" + 
+                           "Sometimes characters like parentheses (sometimes called brackets) don't work correctly.\r\n" + 
+                           "Escape sequences require us to use a backslash '\\' in order to output these special characters.");
+    }
+}
