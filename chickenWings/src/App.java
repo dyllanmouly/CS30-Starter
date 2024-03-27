bddbbb827794cfa88e0de1a36be4ceacda0bd71c
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-03-27 11:56:43 -0600

    chickenWings
    yummy

---
+++ chickenWings/src/App.java
@@ -0,0 +1,16 @@
+// Program name: chickenWings
+// Student name: Dyllan Mouly
+// Date: March 27. 2024 
+// Program Description: chicken wings :) 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double chickenCost = 0.75;
+        double totalMoney = 4.75;
+
+        double wingPurchase = (int)(totalMoney / chickenCost);
+       
+        System.out.println("The total number of wings you can buy is " + wingPurchase);
+
+    }
+}
