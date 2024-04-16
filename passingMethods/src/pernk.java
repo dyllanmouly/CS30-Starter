07890e1b4775515fc829b132907bdc163f8c2b6b
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-04-16 11:55:19 -0600

    passingMethods
    pernk

---
+++ passingMethods/src/pernk.java
@@ -0,0 +1,12 @@
+public class pernk {
+
+        public void ernk()
+        {
+            System.out.println("hi guys :) ");
+        }
+
+        public void ernk2()
+        {
+            System.out.println("bye guys :(");
+        }
+}
---
+++ passingMethods/src/pernkRunner.java
@@ -0,0 +1,8 @@
+public class pernkRunner {
+    public static void main(String[] args) throws Exception {
+        pernk say = new pernk();
+
+        say.ernk();
+        say.ernk2();
+    }
+}
