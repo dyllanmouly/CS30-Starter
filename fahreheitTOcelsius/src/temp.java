cbb3a10a088abfedb09a5e8edb84a59848a9452d
dyllanmouly <159061422+dyllanmouly@users.noreply.github.com>
2024-04-22 12:08:17 -0600

    far

--- fahreheitTOcelsius/src/temp.java
+++ fahreheitTOcelsius/src/temp.java
@@ -13,19 +13,19 @@ public class temp {
     public int tempType()
     {
         Scanner keys = new Scanner(System.in);
-        System.out.println("Is that temperature in Fahrenheit(1) or Celsius(2)?");
+        System.out.println("Do you want to convert that to Fahrenheit(1) or Celsius(2)?");
         int tempPicked = keys.nextInt();
         
         return tempPicked;
     }
 
     public void fahrenheitConvert()
-    {
+    {   //celsius into fahrenheit
         System.out.println(1.8 * temp + 32);
     }
 
     public void celsiusConvert()
-    {
+    {   //fahrenheit into celsius
         System.out.println(0.56 * (temp - 32));
     }
 
